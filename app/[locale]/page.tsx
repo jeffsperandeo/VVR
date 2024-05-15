9c64da6efc4faa2ab3b6c9cde58b65b2d5174509
jeffsperandeo <email@jeffsperandeo.com>
2024-05-15 07:04:58 -0500

    added video to startchatting page

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -9,15 +9,24 @@ export default function HomePage() {
   const { theme } = useTheme()
 
   return (
-    <div className="flex size-full flex-col items-center justify-center">
-      <div>
+    <div className="relative flex size-full flex-col items-center justify-center">
+      <video
+        className="absolute left-0 top-0 size-full object-cover"
+        autoPlay
+        loop
+        muted
+      >
+        <source src="../../components/video/aetherback.mp4" type="video/mp4" />
+      </video>
+
+      <div className="relative z-10">
         <AfogSVG theme={theme === "dark" ? "dark" : "light"} scale={0.3} />
       </div>
 
-      <div className="mt-2 text-4xl font-bold">aetherframe</div>
+      <div className="relative z-10 mt-2 text-4xl font-bold">aetherframe</div>
 
       <Link
-        className="mt-4 flex w-[200px] items-center justify-center rounded-md bg-blue-500 p-2 font-semibold"
+        className="relative z-10 mt-4 flex w-[200px] items-center justify-center rounded-md bg-blue-500 p-2 font-semibold"
         href="/login"
       >
         Start Chatting
